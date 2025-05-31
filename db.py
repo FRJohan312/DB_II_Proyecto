76ea0b1547dca875d84d81f2731a68ed8f0c0277
Tu Nombre <johan37021@gmail.com>
2025-05-31 12:56:50 -0500

    Seguridad

--- db.py
+++ db.py
@@ -9,5 +9,5 @@ contrasena = os.getenv("password")
 DB_NAME = os.getenv("db")
 
 def get_db():
-    client = MongoClient(f"mongodb+srv://{username}:{contrasena}@cluster0.hhohkzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    client = MongoClient(f"mongodb+srv://johangomez1:{contrasena}@cluster0.hhohkzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     return client[DB_NAME]
